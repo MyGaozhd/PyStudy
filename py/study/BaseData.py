89d9cafcb9bbca8153f79b84b8c2ac79ee22f553
高志东 <gaozhdf@yonyou.com>
2019-11-19 19:36:28 +0800

    初次提交，python的基础数据类型demo

---
+++ py/study/BaseData.py
@@ -0,0 +1,55 @@
+print("ggg")
+print("ggg")
+print("ggg")
+print("ggg")
+print("ggg")
+print("ggg")
+
+"""
+数值型的变量使用
+"""
+a = 1
+b = 1.1
+c = 999999.6
+print(a + b + c)
+
+"""
+字符串的使用
+"""
+kk = "程序员"
+print(kk)
+string1 = "qwqewqrwerwqerqw"
+print(string1[0:5])
+
+print(string1[0:5] * 2)
+
+"""
+列表的使用
+"""
+list1 = ["abd", 123, 456]
+
+print(list1)
+list1.append("pdoffjid")
+print(list1[1:3])
+
+"""
+元组的使用
+"""
+yz = ("sad", "wrwe", "fdsfre", 2143)
+print(yz)
+
+"""
+字典的使用
+"""
+dic = {"kkkk": "llllll", "llll": "kkkk"}
+
+print(dic)
+print(dic['kkkk'])
+
+print(dic.keys())
+
+print(dic.values())
+
+dic['kkkk'] = 'uuuu'
+print(dic)
+
