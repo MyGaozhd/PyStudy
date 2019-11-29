86afd8c13625b44b6797aecbe4827d0a039557bf
高志东 <gaozhdf@yonyou.com>
2019-11-29 16:21:57 +0800

    循环使用

--- py/study/forwhile.py
+++ py/study/forwhile.py
@@ -20,6 +20,13 @@ let = ["我", "今年", 18]
 for k in let:
     print(k)
 
-
-
 print("结束for循环", "结束")
+
+print("开始 - - - 索引 - - - for循环", "开始")
+
+fruits = ["banana", "apple", "orange"]
+
+for index in range(len(fruits)):
+    print(fruits[index])
+
+print("结束 - - - 索引 - - - for循环", "结束")
