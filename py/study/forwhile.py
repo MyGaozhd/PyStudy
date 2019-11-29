0eeb97d09c6dd23c01f7036a14baf0a847263b9a
高志东 <gaozhdf@yonyou.com>
2019-11-29 16:14:38 +0800

    循环使用

---
+++ py/study/forwhile.py
@@ -0,0 +1,25 @@
+print("开始while循环")
+
+i = 0
+while i < 9:
+    print("当前值：", i)
+    i += 1
+else:
+    print("跳出循环:", "当前值：", i, "大于9")
+print("结束while循环")
+
+print()
+
+print("开始for循环", "开始")
+
+for letter in "python":
+    print("当前字母是：", letter)
+
+let = ["我", "今年", 18]
+
+for k in let:
+    print(k)
+
+
+
+print("结束for循环", "结束")
